12abe39e7e4a28fe984beb64be65544a876b73b9
jvelazquezs <jorgel.velazquezs@gmail.com>
2021-01-07 17:14:12 -0700

    2020-01-07

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,16 @@
 #
 #!/bin/sh -e
 
+host="$1"
+shift
+cmd="$@"
+  
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "Postgres is up - executing command"
+
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
--- run.sh
+++ run.sh
@@ -2,19 +2,22 @@
 
 set -e
 
-if [[ $# -ne 4 ]]; then
-    echo "USAGE: ./run.sh REGISTRY_IP REGISTRY_USER REGISTRY_PASSWORD SERVER_IP"
+if [[ $# -ne 1 ]]; then
+    echo "USAGE: ./run.sh SERVER_IP"
     exit 1
 fi
 
-$REG_IP=$1
-$USER=$2
-$PASSWORD=$3
-$SERV_IP=$4
+if ! groups $USER | grep -e root -e wheel -q; then
+    echo "User $USER not allowed to run yum or become root in ansible playbook"
+    exit 1
+fi
 
-docker image build . --tag $REG_IP:5000/django-demo:1.0
-echo $PASSWORD | docker login --username $USER --password-stdin
-docker image push $REG_IP:5000/django-demo:1.0
-ansible-playbook -i 127.0.0.1, playbook.yml
+SERV_IP=$1
 
+sudo yum install -y epel-release
+sudo yum install -y ansible
+ansible-playbook -i 127.0.0.1, playbook.yml -e address=$SERV_IP
+
+# The frontend container takes a while to start as it waits for a successful Postgres connection
+sleep 10
 curl http://$SERV_IP:8000/myapp/
