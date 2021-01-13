e2b31f91fa2b1a049482481eb30968785dd6ed5e
jvelazquezs <jorgel.velazquezs@gmail.com>
2021-01-12 17:19:23 -0700

    changes to run.sh

--- run.sh
+++ run.sh
@@ -20,11 +20,19 @@ ansible-playbook -i 127.0.0.1, playbook.yml -e address=$SERV_IP
 
 # The frontend container takes a while to start as it waits for a successful Postgres connection
 RESPOSE_CODE="000"
+START_TIME="$(date +%s)"
+ELAPSED_TIME=0
 
 while [ "$RESPONSE_CODE" != "200" ]; do
-    echo "Starting App Stack ..."
+    echo "Starting App Stack ... $ELAPSED_TIME"
     RESPONSE_CODE=$(python test_connection.py $SERV_IP)
     sleep 10
+    ELAPSED_TIME=$[ $(date +%s) - $START_TIME ]
+
+    if [ $ELAPSED_TIME -ge 120 ]; then
+        echo "App Stack is taking more than 120s ..." >&2
+        exit 1
+    fi 
 done
 
 curl http://$SERV_IP:8000/myapp/
